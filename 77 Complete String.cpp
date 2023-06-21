eeecec06aa22dc7e478a717ded18e91c2178ac15
crispy-coffee <devverma712@gmail.com>
2023-06-21 23:47:20 +0530

    Day 18

--- 77 Complete String.cpp
+++ 77 Complete String.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h> 
 string completeString(int n, vector<string> &a){
     // Write your code here.
+    /*
     unordered_map<string,bool> joint;
 
     for(auto i:a){
@@ -21,7 +22,7 @@ string completeString(int n, vector<string> &a){
             answer.push_back(a[i]);
         }
     }
-    
+
     sort(begin(answer), end(answer), [&](string& a, string& b){
         if(a.size() == b.size()){
             return a<b;
@@ -33,5 +34,42 @@ string completeString(int n, vector<string> &a){
         return "None";
     }
     return answer[0];
+    */
+
+   //Little Optimize
+   unordered_map<string,bool> joint;
+
+    for(auto i:a){
+        joint[i] = true;
+    }
+
+    string answer;
+
+    for(int i=0; i<n; i++){
+        bool valid = true;
+        for(int j=0; j<a[i].size(); j++){
+            if(joint.find(a[i].substr(0,j+1)) == joint.end()){
+                valid = false;
+                break;
+            }
+        }
+        if(valid){
+            
+            if(answer.size() == 0){
+                answer = a[i];
+            }else if(answer.size() == a[i].size()){
+                answer = min(answer, a[i]);
+            }else if(answer.size()<a[i].size()){
+                answer = a[i];
+            }
+
+        }
+    }
+
+    if(answer.size() == 0){
+        return "None";
+    }
+    return answer;
+
 
 }   
