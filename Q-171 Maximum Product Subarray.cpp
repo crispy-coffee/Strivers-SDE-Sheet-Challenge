d2019d414d6fb31d44a1d72732ea45e9857762f7
crispy-coffee <devverma712@gmail.com>
2023-07-16 22:54:37 +0530

    Day 41

---
+++ Q-171 Maximum Product Subarray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+int maximumProduct(vector<int> &arr, int n)
+{
+	
+	int product = 1;
+	int maxi = INT_MIN;
+	for(int i=0; i<n; i++){
+		product *= arr[i];
+		maxi = max(maxi, product);
+		if(product == 0)
+			product = 1;
+	}
+
+	product = 1;
+	
+	for(int i=n-1; i>=0; i--){
+		product *= arr[i];
+		maxi = max(maxi, product);
+		if(product == 0)
+			product = 1;
+	}
+	
+	return maxi;
+}
